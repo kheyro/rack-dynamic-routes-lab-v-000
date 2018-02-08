563d95bac48bca26d91cdfc0888b720b1846abd5
Denis Atkesone <denis.atkesone@gmail.com>
2018-02-08 19:12:38 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path.match(/items)
+    if req.path.match(/items/)
     else
       resp.write "Route not found"
       resp.status = 404
